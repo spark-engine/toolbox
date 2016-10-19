da1b825417012c8a5591807e14d50c6df7b8beb4
Brandon Mathis <brandon@imathis.com>
2016-10-19 16:35:11 -0500

    Reorganized libs a bit, added debounce and throttle, added tests for throttle

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-require('./shims/_classlist')
-require('./shims/_object.assign.js')
+require('./lib/shims/_classlist')
+require('./lib/shims/_object.assign.js')
+var timing = require('./lib/timing.js')
 
 var Toolbox = {
-  getClosest: function ( elem, selector ) {
-    for ( ; elem && elem !== document; elem = elem.parentNode ) {
-      if ( self.matches( elem, selector ) ) return elem;
+
+  // Get closest DOM element up the tree that matches a given selector
+  getClosest: function ( el, selector ) {
+    for ( ; el && el !== document; el = el.parentNode ) {
+      if ( self.matches( el, selector ) ) return el;
     }
     return false;
   },
 
+  // Get next DOM element that matches a given selector
+  getNext: function( el, selector ) {
+    for ( ; el && el !== document; el = el.parentNode ) {
+      if ( el.querySelector( selector ) ) return el.querySelector( selector );
+    }
+    return false;
+  },
+
+  // Matches selector function
+  // @reference https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
   matches: function ( el, selector ) {
     return ( el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector ).call( el, selector );
-  }
+  },
+
+  throttle: timing.throttle,
+
+  debounce: timing.debounce
+  
 }
 
 
--- test/test.js
+++ test/test.js
@@ -1,33 +1,51 @@
 var assert = require('chai').assert
 var toolbox = require('../')
 
+var injectHTML = function(html) {
+  document.body.insertAdjacentHTML('beforeend', html)
+  return document.body.lastChild
+}
+
+var now = function() {
+  return Date.now();
+}
+
 describe( 'Toolbox', function(){
 
-  describe( '.getClosest', function() {
-    document.body.insertAdjacentHTML('beforeend', "<p class='grandpa person'><span class='dad person'><span class='cub'></span></span></p>")
+  afterEach( function() {
 
-    var cub = document.querySelector('.cub')
-    var dad = document.querySelector('.dad')
-    var grandpa = document.querySelector('.grandpa')
-
-    it('finds closest element', function(){
-      assert.equal( toolbox.getClosest( cub, 'p' ), grandpa )
+    // Clean up DOM
+    Array.prototype.forEach.call( document.querySelectorAll('body > *'), function ( node ) { 
+      document.body.removeChild(node)
     })
 
-    it('finds closest class', function(){
-      assert.equal( toolbox.getClosest( cub, '.person' ), dad )
-    })
-
-    it('finds closest class', function(){
-      assert.notEqual( toolbox.getClosest( cub, '.grandpa' ), dad )
-    })
-
-    document.body.removeChild(grandpa)
   })
 
+  describe( '.getClosest', function() {
+
+    injectHTML( "<p class='outer widget'><span class='middle widget'><span class='inner'></span></span></p>" )
+
+    var outer = document.querySelector('.outer')
+    var middle = document.querySelector('.middle')
+    var inner = document.querySelector('.inner')
+
+    it('finds closest element', function(){
+      assert.equal( toolbox.getClosest( inner, 'p' ), outer )
+    })
+
+    it('finds closest class', function(){
+      assert.equal( toolbox.getClosest( inner, '.widget' ), middle )
+    })
+
+    it('finds closest class', function(){
+      assert.notEqual( toolbox.getClosest( inner, '.outer' ), middle )
+    })
+
+  })
 
   describe( '.matches', function() {
-    document.body.insertAdjacentHTML('beforeend', "<div id='food' class='toast bread' data-yummy='yes'></div>")
+
+    injectHTML( "<div id='food' class='toast bread' data-yummy='yes'></div>" )
 
     div = document.querySelector('#food')
 
@@ -46,6 +64,7 @@ describe( 'Toolbox', function(){
       assert.isTrue( toolbox.matches( div, '#food' ) )
       assert.isFalse( toolbox.matches( div, '#garbage' ) )
     })
+
   })
 
   describe( 'Object.assign', function() {
@@ -55,11 +74,14 @@ describe( 'Toolbox', function(){
     var hashB = { a: 'good', b: 'awesome' }
     var originHashB = hashB
 
-    var testObject = Object.assign( {}, hashA, hashB )
+    it('merges hashes', function() {
 
-    assert.equal( originHashA, hashA )
-    assert.equal( originHashB, hashB )
-    assert.equal( testObject.a, 'good' )
+      var testObject = Object.assign( {}, hashA, hashB )
+
+      assert.equal( originHashA, hashA )
+      assert.equal( originHashB, hashB )
+      assert.equal( testObject.a, 'good' )
+    })
 
   })
 
@@ -68,13 +90,55 @@ describe( 'Toolbox', function(){
     document.body.insertAdjacentHTML('beforeend', "<div class='toast'></div>")
     var el = document.querySelector('.toast')
 
-    el.classList.add( 'bread' )
-    assert.equal( el.className, 'toast bread' )
+    it('adds classes', function() {
+      el.classList.add( 'bread' )
+      assert.equal( el.className, 'toast bread' )
+    })
 
-    el.classList.toggle( 'bread' )
-    assert.equal( el.className, 'toast' )
+    it('toggles classes', function() {
+      el.classList.toggle( 'bread' )
+      assert.equal( el.className, 'toast' )
+    })
 
-    assert.isTrue( el.classList.contains('toast') )
+    it('checks classnames', function() {
+      assert.isTrue( el.classList.contains('toast') )
+      assert.isFalse( el.classList.contains('elves') )
+    })
+
+  })
+
+  describe( 'debounce', function() {
+
+    count = injectHTML( "<div id='count'>0</div>" )
+
+    var counter = 0
+    var increment = function() {
+      counter += 1
+      count.textContent = counter
+    }
+
+    var add = new Event('add')
+    var trigger = function(timeout) {
+      setTimeout( function(){ document.body.dispatchEvent(add); }, timeout )
+    }
+
+    var debouncedIncrement = toolbox.debounce( increment, 5, { trailing: true })
+    document.body.addEventListener('add', debouncedIncrement)
+
+    it('only fires at the correct intervals', function(done) {
+      trigger(1)
+      trigger(2)    // should fire
+      trigger(10)   // should fire
+      trigger(11)   // should not fire because it hasn't been long enough since last event
+      trigger(16)   // should fire
+
+      setTimeout(function() {
+
+        assert.equal( count.textContent, '3' )
+        done()
+
+      }, 30)
+    })
 
   })
 
