ee4d5c1c9dcdbb4cc24c482bcec0a84568f0b981
Brandon Mathis <brandon@imathis.com>
2018-04-25 17:24:17 -0500

    Fixed time.parse returning false on invalid values

--- test/time.test.js
+++ test/time.test.js
@@ -1,9 +1,15 @@
 var time = require('../').time
-
-var date = time.parse('1993-10-12 05:34:21')
+var DateTime = require( 'luxon' ).DateTime
 
 test( 'parses a time', () => {
-  expect( date.day ).toBe( 12 )
+  expect( time.parse('1993-10-12 05:34:21') ).toBeTruthy()
+  expect( time.parse('1993-10-12 05:34:21').day ).toBe( 12 )
+  expect( time.parse('2018-04-25T22:15:21.796Z').day ).toBe( 25 )
+  expect( time.parse('1524694729228').day ).toBe( 25 )
+})
+
+test( 'rejects an invalid time', () => {
+  expect( time.parse('test') ).toBeFalsy()
 })
 
 test( 'compares times', () => {
